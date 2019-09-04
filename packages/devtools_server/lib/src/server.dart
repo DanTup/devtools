a313fe73def394520609a298dabcfbaf0c9dbf82
Danny Tuppeny <dantup@google.com>
2019-09-04 18:19:47 +0100

    Add basic test for DevTools connecting back to SSE endpoint

--- packages/devtools_server/lib/src/server.dart
+++ packages/devtools_server/lib/src/server.dart
@@ -154,6 +154,9 @@ Future<HttpServer> serveDevTools({
         case 'vm.register':
           await _handleVmRegister(id, params, machineMode, devToolsUrl);
           break;
+        case 'client.list':
+          await _handleClientsList(id, params, machineMode);
+          break;
         default:
           printOutput(
             'Unknown method ${json['method']}',
@@ -206,6 +209,29 @@ Future<void> _handleVmRegister(dynamic id, Map<String, dynamic> params,
       machineMode: machineMode,
     );
   }
+}
+
+Future<void> _handleClientsList(
+    dynamic id, Map<String, dynamic> params, bool machineMode) async {
+  final connectedClients = clients.connectedClients;
+  printOutput(
+    connectedClients
+        .map((c) =>
+            '${c.hasConnection.toString().padRight(5, ' ')} ${c.vmServiceUri.toString()}')
+        .join('\n'),
+    {
+      'id': id,
+      'result': {
+        'clients': connectedClients
+            .map((c) => {
+                  'hasConnection': c.hasConnection,
+                  'vmServiceUri': c.vmServiceUri.toString(),
+                })
+            .toList()
+      },
+    },
+    machineMode: machineMode,
+  );
 }
 
 Future<bool> _tryReuseExistingDevToolsInstance(Uri vmServiceUri) async {
